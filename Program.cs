342d2fdceef512e8e673c5a66d7dcdab69c2cb57
sai6kumar6 <sai6kumar6@hotmail.com>
2024-05-28 17:05:56 +0530

    Update Program.cs
    Added one more line

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp22
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Welcome");
             Console.ReadLine();
         }
     }
